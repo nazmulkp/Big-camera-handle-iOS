8197743dd617ecdf8ea83df88918683a4808f799
Nazmul Hasan <nazmulcsharp@gmail.com>
2025-11-16 23:33:00 +0600

    Initial Commit

---
+++ CameraAppSwiftUI/CameraAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppSwiftUIApp.swift
+//  CameraAppSwiftUI
+//
+//  Created by Md Nazmul Hasan on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
